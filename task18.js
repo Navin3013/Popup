a3e6d36b7cef943464f8881a03e560343cc74daf
Navin3013 <navinrajpns3013@gamil.com>
2025-02-14 23:03:16 -0800

    popup sample

---
+++ task18.js
@@ -0,0 +1,13 @@
+
+function clic() {
+    console.log("hello");
+        // document.getElementById("popup").style.transform="translateY(0px)";
+        document.getElementById("popup").style.top = "250px";
+    }; 
+
+
+    function clos() {
+        console.log("hello");
+            // document.getElementById("popup").style.transform="translateY(-610px)";
+            document.getElementById("popup").style.top = "-210px";
+        }; 
